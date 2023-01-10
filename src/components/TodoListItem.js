9d875cb74d305d3c8b5e4c2a705dbb229ba32673
suzzeong <su981006@naver.com>
2023-01-10 13:59:36 +0900

    feat: 기능 구현하기

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -3,18 +3,21 @@ import {
   MdCheckBoxOutlineBlank,
   MdCheckBox,
   MdRemoveCircleOutline
-} from 'react-icons/md'
-import './TodoListItem.scss'
+} from 'react-icons/md';
+import './TodoListItem.scss';
+import cn from 'classnames';
 
-const TodoListItem = () => {
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
+  const { id, text, checked } = todo;
+
   return (
     <div className='TodoListItem'>
-      <div className='checkbox'>
-        <MdCheckBoxOutlineBlank/>
-        <div className='text'>할 일</div>
+      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className='text'>{text}</div>
       </div>
-      <div className='remove'>
-        <MdRemoveCircleOutline/>
+      <div className='remove' onClick={() => onRemove(id)}>
+        <MdRemoveCircleOutline />
       </div>
     </div>
   );
